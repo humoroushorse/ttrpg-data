f14dc06ef68a9007fbd98de1dfcd3de8495d2f99
Ian Kirkpatrick <iankirkpatrick@metalbackgorilla.com>
2022-10-10 17:56:14 -0600

    init - spells (pbh, xanathar, tasha) and their classes

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# dnd-data
-just random data I may or may not use
+# DnD: Data
+
+## staging
+
+Scripts
+* `python3 manual_script.py` helper for manual
+* `python3 manual_script.py` helper for raw -> parsed
+
+### data_raw
+Data sources
+
+* [phb.json](https://www.dropbox.com/sh/mynr6seqj4uelyv/AACDV9wlChamhsIHq1UdvYlBa/Spell%20Library%2011-16-19.JSON?dl=0)
+* [tashas.json](dropbox.com/s/vw2h0houum66vxz/TCoE_Spells_SummonStats.json?dl=0)
+* [xanathar.json](https://www.dropbox.com/s/0yxep5f344di2zq/XGtE%20Spells.JSON?dl=0)
+
+Where phb and xanathar came from [this dropbox library](https://www.dropbox.com/sh/mynr6seqj4uelyv/AAAUxNI2-lY16XAq7am4Ujhja?dl=0)
+
+### data_parsed
+Data closer to the form that we need
+
+### data_manual
+Data plucked from data_parsed manually and then transformed further
---
+++ parse_script.py
@@ -0,0 +1,77 @@
+import json
+from itertools import groupby
+from operator import itemgetter
+from datetime import datetime
+
+file = 'spells_tashas'
+
+with open(f"data_raw/{file}.json", "r") as input:
+  as_json = json.load(input)
+  
+  as_list = [v for _, v in as_json.items()]
+
+  # IF JSON STRING
+  first_element = as_list[0]
+  if type(first_element) is str:
+    as_list = [json.loads(v) for v in as_list]
+
+  # well one of those items is actually a list
+  as_list_dicts = [item for item in as_list if type(item) is dict]
+  # this should be our one non-dict
+  as_list_non_dicts = [item for item in as_list if type(item) is not dict]
+
+  grouped_dict = {}
+  datetime_now = f"{datetime.now()}"
+  for d in as_list_dicts:
+    d = {
+      'id': d.get('Id').lower(),
+      'version': d.get('Version'),
+      'name': d.get('Name').lower(),
+      'path': d.get('Path') if d.get('Path') else None,
+      'source': d.get('Source') if d.get('Source') else None,
+      'source_id': -1, # TODO: id based on source for links
+      'casting_time': d.get('CastingTime'),
+      'classes': [c.lower() for c in d.get('Classes', [])],
+      'components': d.get('Components', '').lower(),
+      'description': d.get('Description'),
+      'duration': d.get('Duration', '').lower(),
+      'level': d.get('Level'),
+      'range': d.get('Range', '').lower(),
+      'ritual': d.get('Ritual'),
+      'school': d.get('School', '').lower(),
+      'last_updated': datetime_now,
+      'created_at': datetime_now,
+      'created_by': 'admin:system',
+      'updated_by': 'admin:system',
+      'homebrew': False,
+    }
+    source = d.get('source')
+    level = d.get('level')
+    if type(level) is str:
+      level = int(level)
+    if source not in grouped_dict.keys():
+      grouped_dict[source] = {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: []
+      }
+      grouped_dict[source][level] = [d]
+    elif level not in grouped_dict[source].keys():
+      grouped_dict[source][level] = [d]
+    else:
+      grouped_dict[source][level].append(d)  
+  for key, value in grouped_dict.items():
+    print(key if key else '>>>>>> NO NAME <<<<<<')
+    for k, v in value.items():
+      # print(f"\t{k}: {len(v)}")
+      grouped_dict[key][k] = sorted(v, key=lambda x: x.get('name'))
+    
+with open(f"data_parsed/{file}.json", "w") as output:
+  output.write(json.dumps(grouped_dict))
